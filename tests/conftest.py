9ad05b5e75607f7cac2cd3724fbebe92b3ac1e53
Mexriddin Maxkamtayev <maxkamtayev@gmail.com>
2024-02-27 15:02:20 +0500

    add mode run in browserstack

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,59 @@
 import pytest
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
+from appium.options.android import UiAutomator2Options
+from dotenv import dotenv_values
 
+
+config = dotenv_values(".env")
 app_package = 'com.saucelabs.mydemoapp.rn'
 
 
-@pytest.fixture(scope='session', autouse=True)
-def appium_driver():
-    capabilities = {
-        'platformName': 'android',
-        'deviceName': '@Pixel_3a_API_34',
-        'appium:automationName': 'UIAutomator2',
-        'appium:app': 'C:\\Users\\mexriddin maxkamtaev\\PycharmProjects\\appium-homework-pr\\midterm_project\\app'
-                      '\\MyDemoApp.apk'
+def pytest_addoption(parser):
+    parser.addoption("--mode_run", action="store", default="local_em", help="Choose to run mode: local_em, remote_bs")
 
-    }
-    options = AppiumOptions().load_capabilities(capabilities)
-    appium_driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
+
+
+
+@pytest.fixture(scope='session', autouse=True)
+def appium_driver(request):
+    appium_driver = None
+    mode_run = request.config.getoption("--mode_run")
+    if mode_run == "local_em":
+        capabilities = {
+            'platformName': 'android',
+            'deviceName': '@Pixel_3a_API_34',
+            'appium:automationName': 'UIAutomator2',
+            'appium:app': 'C:\\Users\\mexriddin maxkamtaev\\PycharmProjects\\appium-homework-pr\\midterm_project\\app'
+                          '\\MyDemoApp.apk'
+        }
+        options = AppiumOptions().load_capabilities(capabilities)
+        appium_driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
+
+
+    elif mode_run == "remote_bs":
+        capabilities = {
+            'platformName': 'android',
+            'platformVersion': '9.0',
+            'deviceName': 'Google Pixel 3a',
+            "app": "bs://7a04d670d0ee20c9499b17c676354d0f3243151d",
+
+            'bstack:options': {
+                "projectName": "My Demo Project",
+                "buildName": "browserstack-build-2",
+                "sessionName": "BStack first_test",
+
+                # Set your access credentials
+                "userName": config['BROWSERSTACK_USERNAME'],
+                "accessKey": config['BROWSERSTACK_ACCESS_KEY']
+            }
+
+        }
+        options = UiAutomator2Options().load_capabilities(capabilities)
+        appium_driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+
     print("\nStarting appium..")
     yield appium_driver
-    # print("Removing app...")
-    # appium_driver.remove_app(app_package)
     print("Quitting appium...")
     appium_driver.quit()
 
